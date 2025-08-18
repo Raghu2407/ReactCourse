fa76b6e6dd6f64b42f017e8142fb044221ae8e6f
Raghu2407 <raghoothamkalkeri96@gmail.com>
2025-08-18 13:09:27 +0530

    updates

--- reactcourse/vite.config.js
+++ reactcourse/vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: "https://github.com/Raghu2407/ReactCourse.git"
+  base: "https://raghu2407.github.io/ReactCourse/"
 })
